cf77702538eae2a8b2304a98ed8aaf2529bccb7f
aden trisna daud kurnia <aden.trisna@exaditama.id>
2020-04-13 15:00:15 +0700

    reinit project, base ui

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+     <v-app-bar
+      app
+      color="indigo"
+      dark
+    >
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
---
+++ src/layouts/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+    <main style="app-main">
+      <app-sidebar></app-sidebar>
+      <app-header></app-header>
+      <v-content>
+        <router-view></router-view>
+      </v-content>
+    </main>
+  </v-app>
+</template>
+
+<script>
+import AppHeader from '@/components/AppHeader'
+import AppSidebar from '@/components/AppSidebar'
+export default {
+  name: 'AppHomeLayout',
+  components: {
+    AppSidebar,
+    AppHeader
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+app-main {
+  display: flex;
+  flex-direction: row;
+  .app-content {
+    position: relative;
+    margin-left: 300px;
+    min-height: 100vh;
+    background-color: #faa;
+  }
+}
+</style>
