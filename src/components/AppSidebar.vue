cbcf4034bc9209452cf9c5688f08f0ac1d116ac7
aden trisna daud kurnia <aden.trisna@exaditama.id>
2020-04-13 17:09:50 +0700

    change icon color

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -1,58 +1,58 @@
 <template>
   <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant.sync="mini"
-      app
-    >
-      <v-list dense>
-        <v-list-item style="border-bottom: 1px solid rgba(0, 0, 0, 0.05);" class="pb-2">
-          <v-list-item-icon>
-            <div v-if="mini" style="margin-top: -5px; margin-left: 5px">
-              <span style="font-weight: 900; font-size: 24px; color:#F5B13C;">e</span>
-            </div>
-            <v-img v-else
-              class="mt-1"
-              :src="eatmoreLogo"
-              height="21"
-            />
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title></v-list-item-title>
-          </v-list-item-content>
-          <v-list-item-icon>
-            <v-btn @click="mini = !mini" fab x-small elevation="0" outlined color="#DADADA">
-              <v-icon>mdi-menu</v-icon>
-            </v-btn>
+    v-model="drawer"
+    :mini-variant.sync="mini"
+    app
+  >
+    <v-list dense>
+      <v-list-item style="border-bottom: 1px solid rgba(0, 0, 0, 0.05);" class="pb-2">
+        <v-list-item-icon>
+          <div v-if="mini" style="margin-top: -5px; margin-left: 5px">
+            <span style="font-weight: 900; font-size: 24px; color:#F5B13C;">e</span>
+          </div>
+          <v-img v-else
+            class="mt-1"
+            :src="eatmoreLogo"
+            height="21"
+          />
         </v-list-item-icon>
-        </v-list-item>
-      </v-list>
+        <v-list-item-content>
+          <v-list-item-title></v-list-item-title>
+        </v-list-item-content>
+        <v-list-item-icon>
+          <v-btn @click="mini = !mini" fab x-small elevation="0" outlined color="#DADADA">
+            <v-icon>mdi-menu</v-icon>
+          </v-btn>
+      </v-list-item-icon>
+      </v-list-item>
+    </v-list>
 
-      <v-list dense>
-        <v-list-item link
-          v-for="(sidebarItem, i) in sidebarData"
-          :key="i"
-          >
-          <v-list-item-icon>
-            <v-icon>{{sidebarItem.icon}}</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>{{sidebarItem.name}}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-        <v-subheader class="pl-4 text-center">Main Menu</v-subheader>
-        <v-list-item link
-          v-for="(mainMenuItem, i) in mainMenuData"
-          :key="i"
-          >
-          <v-list-item-icon>
-            <v-icon>{{mainMenuItem.icon}}</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>{{mainMenuItem.name}}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+    <v-list dense>
+      <v-list-item link
+        v-for="(sidebarItem, i) in sidebarData"
+        :key="i"
+        >
+        <v-list-item-icon>
+          <v-icon color="#FDB526">{{sidebarItem.icon}}</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>{{sidebarItem.name}}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+      <v-subheader class="pl-4 text-center">Main Menu</v-subheader>
+      <v-list-item link
+        v-for="(mainMenuItem, i) in mainMenuData"
+        :key="i"
+        >
+        <v-list-item-icon>
+          <v-icon color="#FDB526">{{mainMenuItem.icon}}</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>{{mainMenuItem.name}}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
 <script>
--- src/router/nav.js
+++ src/router/nav.js
@@ -11,30 +11,39 @@ export const sidebar = [
 
 export const mainMenu = [
   {
+    icon: 'mdi-file-pdf',
     name: 'Report'
   },
   {
+    icon: 'mdi-layers',
     name: 'Products'
   },
   {
+    icon: 'mdi-silverware',
     name: 'Inventory'
   },
   {
+    icon: 'mdi-timetable',
     name: 'Promo'
   },
   {
+    icon: 'mdi-account-circle',
     name: 'Customer'
   },
   {
+    icon: 'mdi-account-box',
     name: 'Employees'
   },
   {
+    icon: 'mdi-deskphone',
     name: 'Accounting'
   },
   {
+    icon: 'mdi-table',
     name: 'Table Management'
   },
   {
+    icon: 'mdi-settings',
     name: 'System'
   }
 ]
