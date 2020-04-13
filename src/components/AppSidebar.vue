72e6f4f16370fb1e11f5e2d173c5875711972002
aden trisna daud kurnia <aden.trisna@exaditama.id>
2020-04-13 16:17:11 +0700

    fix

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -1,24 +1,25 @@
 <template>
   <v-navigation-drawer
       v-model="drawer"
+      :mini-variant.sync="mini"
       app
     >
       <v-list dense>
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-home</v-icon>
-          </v-list-item-action>
+        <v-list-item style="border-bottom: 1px solid rgba(0, 0, 0, 0.05);" class="pb-2">
+          <v-list-item-icon>
+            <v-img
+              :src="eatmoreLogo"
+              height="21"
+            />
+          </v-list-item-icon>
           <v-list-item-content>
-            <v-list-item-title>Home</v-list-item-title>
+            <v-list-item-title></v-list-item-title>
           </v-list-item-content>
-        </v-list-item>
-        <v-list-item link>
-          <v-list-item-action>
-            <v-icon>mdi-contact-mail</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Contact</v-list-item-title>
-          </v-list-item-content>
+          <v-list-item-icon>
+            <v-btn @click="mini = !mini" fab x-small elevation="0" outlined color="#DADADA">
+              <v-icon>mdi-menu</v-icon>
+            </v-btn>
+        </v-list-item-icon>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
@@ -29,8 +30,16 @@ export default {
   name: 'AppSidebar',
   data () {
     return {
-      drawer: true
+      drawer: true,
+      mini: false
+    }
+  },
+  computed: {
+    eatmoreLogo () {
+      return require('@/assets/img/Eatmore logo-01 1.png')
     }
   }
 }
 </script>
+
+<style lang="scss"></style>
