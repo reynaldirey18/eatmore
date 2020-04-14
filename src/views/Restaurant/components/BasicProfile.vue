2a94837f14d802ad16d6000492307167c17fd16d
aden trisna daud kurnia <aden.trisna@exaditama.id>
2020-04-14 15:09:46 +0700

    field business name, description, type

--- src/views/Restaurant/components/BasicProfile.vue
+++ src/views/Restaurant/components/BasicProfile.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="pl-4 pt-6">
+    <div class="px-4 pt-6">
         <h1 class="app-title">Basic Profile</h1>
         <form>
             <v-row>
@@ -11,13 +11,39 @@
                     </app-file-upload>
                 </v-col>
                 <v-col cols="9">
-                    <p class="app-subtitle">Business Logo</p>
+                    <p class="app-title-small">Business Logo</p>
                     <p>This logo will appear when customer find your restaurant.</p>
                     <p>Pro tips: Use logo with dimension 1:1</p>
                     <v-btn @click="handleTriggerUpload"><span class="text--primary">Upload Image</span></v-btn>
                     <input class="d-none" type="file" @change="handleFileChange" :accept="accept" ref="file" />
                 </v-col>
             </v-row>
+            <div>
+                <p class="app-title-small" style="margin-bottom: -5px">Business Name</p>
+                <v-text-field
+                    label="Business Name"
+                    placeholder="Eg. floor1 or floor for cafe & resto"
+                    single-line
+                    dense
+                    outlined
+                ></v-text-field>
+            </div>
+            <div>
+                <p class="app-title-small" style="margin-bottom: -5px">Business Description</p>
+                <v-textarea
+                outlined
+                dense
+                placeholder="Eg. All floor smoking area"
+                ></v-textarea>
+            </div>
+            <div>
+                <p class="app-title-small" style="margin-bottom: -5px">Business Type</p>
+                <v-select
+                dense
+                :items="businessTypeList"
+                outlined
+                ></v-select>
+            </div>
         </form>
     </div>
 </template>
@@ -32,6 +58,12 @@ export default {
   data () {
     return {
       accept: '',
+      businessTypeList: [
+        {
+          text: 'Restaurant',
+          value: 1
+        }
+      ],
       businessLogo: null,
       businessName: '',
       businessDescription: '',
