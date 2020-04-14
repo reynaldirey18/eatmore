43572f2188dcdcc1c53f9201710bbe0e413f1b8a
reynaldirey18 <reynaldiranggaprayuda@gmail.com>
2020-04-14 09:08:06 +0700

    progres dashboard

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,14 @@
     <router-view></router-view>
   </div>
 </template>
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Rubik&display=swap');
+body{
+  font-family: 'Rubik', sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 12px;
+  line-height: 20px;
+}
+
+</style>
