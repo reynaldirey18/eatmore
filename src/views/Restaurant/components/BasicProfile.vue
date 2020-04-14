246098489aeff43b51945344ad648bdf27b5653d
aden trisna daud kurnia <aden.trisna@exaditama.id>
2020-04-14 14:45:10 +0700

    upload business logo

--- src/views/Restaurant/components/BasicProfile.vue
+++ src/views/Restaurant/components/BasicProfile.vue
@@ -1,3 +1,58 @@
 <template>
-    <h1 class="app-title">Basic Profile</h1>
+    <div class="pl-4 pt-6">
+        <h1 class="app-title">Basic Profile</h1>
+        <form>
+            <v-row>
+                <v-col cols="3">
+                    <app-file-upload
+                        description="Maximum size 2 mb"
+                        @onFileChange="onFileChange"
+                        :value="businessLogo">
+                    </app-file-upload>
+                </v-col>
+                <v-col cols="9">
+                    <p class="app-subtitle">Business Logo</p>
+                    <p>This logo will appear when customer find your restaurant.</p>
+                    <p>Pro tips: Use logo with dimension 1:1</p>
+                    <v-btn @click="handleTriggerUpload"><span class="text--primary">Upload Image</span></v-btn>
+                    <input class="d-none" type="file" @change="handleFileChange" :accept="accept" ref="file" />
+                </v-col>
+            </v-row>
+        </form>
+    </div>
 </template>
+
+<script>
+import AppFileUpload from '@/components/AppFileUpload'
+export default {
+  name: 'ComponentBasicProfile',
+  components: {
+    AppFileUpload
+  },
+  data () {
+    return {
+      accept: '',
+      businessLogo: null,
+      businessName: '',
+      businessDescription: '',
+      businessType: '',
+      tags: []
+    }
+  },
+  methods: {
+    handleTriggerUpload () {
+      this.$refs.file.click()
+    },
+    handleFileChange (e) {
+      console.log(e)
+      const input = e.target || this.$refs.file
+      const file = input.files ? input.files[0] : null
+
+      this.onFileChange(file)
+    },
+    onFileChange (file) {
+      this.businessLogo = file
+    }
+  }
+}
+</script>
