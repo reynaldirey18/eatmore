1a5bd9f1b5a56cf91a36c0555017c07081977d57
reynaldirey18 <reynaldiranggaprayuda@gmail.com>
2020-04-14 15:23:02 +0700

    chart

---
+++ src/views/Dashboard/component/dayamount.vue
@@ -0,0 +1,74 @@
+<template>
+   <div id="chart">
+        <apexchart type="bar" height="350" :options="chartOptions" :series="series"></apexchart>
+  </div>
+</template>
+<script>
+import VueApexCharts from 'vue-apexcharts'
+// import * as moment from 'moment'
+export default {
+  components: {
+    apexchart: VueApexCharts
+  },
+
+  data: function () {
+    return {
+      moment: null,
+      series: [{
+        name: 'Net Profit',
+        data: [15000, 25000, 35000, 20300, 35000, 21000, 30000]
+      }],
+      chartOptions: {
+        chart: {
+          type: 'bar',
+          height: 350
+        },
+        plotOptions: {
+          bar: {
+            horizontal: false,
+            columnWidth: '35%'
+          }
+        },
+        dataLabels: {
+          enabled: false,
+          style: {
+            colors: ['#3D87F4']
+          }
+        },
+        stroke: {
+          show: true,
+          width: 2,
+          colors: ['transparent']
+        },
+        xaxis: {
+          title: {
+            text: 'Date',
+            style: {
+              fontSize: '14px',
+              fontWeight: 'normal',
+              fontFamily: 'Rubik',
+              color: '#B4B9BF'
+            }
+          },
+          categories: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+        },
+        yaxis: {
+          title: {
+            text: 'Amounts'
+          }
+        },
+        fill: {
+          opacity: 1
+        },
+        tooltip: {
+          y: {
+            formatter: function (val) {
+              return '$ ' + val + ' thousands'
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</script>
---
+++ src/views/Dashboard/component/houramount.vue
@@ -0,0 +1,82 @@
+<template>
+   <div id="chart">
+        <apexchart type="bar" height="350" :options="chartOptions" :series="series"></apexchart>
+  </div>
+</template>
+<script>
+import VueApexCharts from 'vue-apexcharts'
+// import * as moment from 'moment'
+export default {
+  components: {
+    apexchart: VueApexCharts
+  },
+
+  data: function () {
+    return {
+      moment: null,
+      series: [
+        {
+          name: 'Amount',
+          data: [15000, 25000, 35000, 20300, 35000, 21000, 30000, 10000, 20000, 32000, 33000, 35000]
+        },
+        {
+          name: 'Amount',
+          data: [10000, 39000, 40000, 2300, 32000, 18000, 35000, 10000, 23000, 30000, 30000, 34000]
+        }
+      ],
+      chartOptions: {
+        chart: {
+          type: 'bar',
+          height: '350'
+        },
+        colors: ['#DE699A', '#3D87F4'],
+        plotOptions: {
+          bar: {
+            horizontal: false,
+            columnWidth: '35%',
+            columnhight: '100%'
+          }
+        },
+        dataLabels: {
+          enabled: false,
+          style: {
+            colors: ['#DE699A', '#3D87F4']
+          }
+        },
+        stroke: {
+          show: true,
+          width: 2,
+          colors: ['transparent']
+        },
+        xaxis: {
+          title: {
+            text: 'Date',
+            style: {
+              fontSize: '14px',
+              fontWeight: 'normal',
+              fontFamily: 'Rubik',
+              color: '#B4B9BF'
+            }
+          },
+          categories: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+        },
+        yaxis: {
+          title: {
+            text: 'Amounts'
+          }
+        },
+        fill: {
+          opacity: 1
+        },
+        tooltip: {
+          y: {
+            formatter: function (val) {
+              return '$ ' + val + ' thousands'
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</script>
