f4a097dce36c3245de0c521f902ef202d615c8f8
aden trisna daud kurnia <aden.trisna@exaditama.id>
2020-04-15 08:17:17 +0700

    install and register vueGoogleMaps

--- src/main.js
+++ src/main.js
@@ -5,6 +5,15 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 import './assets/scss/main.scss' // Styles: SCSS
 
+import config from './config/config'
+import * as VueGoogleMaps from 'vue2-google-maps'
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: config.google_api_key
+  }
+})
+
 Vue.use(require('vue-moment'))
 
 Vue.config.productionTip = false
