89674172d6938465e4a9831ea0bc7301cf57e801
aden trisna daud kurnia <aden.trisna@exaditama.id>
2020-04-16 12:51:50 +0700

    review

--- src/views/Restaurant/components/Review.vue
+++ src/views/Restaurant/components/Review.vue
@@ -5,8 +5,8 @@
             <v-row>
               <v-col cols="6">
                 <v-card flat>
-                  <v-list color="#F4F5F6" three-line height="108">
-                    <v-list-item>
+                  <v-list color="#F4F5F6" height="108">
+                    <v-list-item three-line>
                       <v-list-item-icon>
                         <v-btn class="ma-2" outlined fab color="#1DBA81">
                           <v-icon>mdi-emoticon-happy</v-icon>
@@ -29,8 +29,8 @@
               </v-col>
               <v-col cols="6">
                 <v-card flat>
-                  <v-list color="#F4F5F6" three-line height="108">
-                    <v-list-item>
+                  <v-list color="#F4F5F6" height="108">
+                    <v-list-item three-line>
                       <v-list-item-icon>
                         <v-btn class="ma-2" outlined fab color="#F32626">
                           <v-icon>mdi-emoticon-sad</v-icon>
@@ -53,5 +53,109 @@
               </v-col>
             </v-row>
         </div>
+         <div>
+            <p class="app-title-small py-3" style="margin-bottom: 0px">All Reviews</p>
+            <div>
+                <v-btn
+                    v-for="(star, i) in starFilterList"
+                    :key="'day' +i"
+                    class="btn-start-filter"
+                    :color="starFilterIndex === i ? '#FDB526' : '#999999'"
+                    :style="starFilterIndex === i ? 'background-color:#FFF8E9': ''"
+                    height="44"
+                    outlined
+                    min-width="104"
+                    @click="starFilterIndex = i"
+                >
+                <v-icon v-text="star.icon" class="mr-1" small></v-icon>
+                <span class="text-capitalize">{{star.name}}</span>
+                </v-btn>
+            </div>
+            <div>
+              <v-list>
+                <v-list-item three-line>
+                  <v-list-item-avatar>
+                    <v-img src="https://c-sf.smule.com/sf/s46/arr/eb/76/150550ba-d611-4e89-99a6-a193e63c4872.jpg"></v-img>
+                  </v-list-item-avatar>
+                  <v-list-item-content>
+                    <v-list-item-title v-text="'Aang'"></v-list-item-title>
+                    <v-list-item-action-text class="d-flex">
+                      <v-rating
+                        :value="4.5"
+                        color="yellow darken-3"
+                        background-color="grey darken-1"
+                        empty-icon="$ratingFull"
+                        half-increments
+                        hover
+                        dense
+                        x-small
+                        readonly
+                      ></v-rating>
+                       <span class="caption small" style="color:#F9A825">
+                        {{ 4.5 }}
+                      </span>
+                    </v-list-item-action-text>
+                    <v-list-item-subtitle class="app-subtitle-small">
+                      Wednesday, 27 April 2020
+                    </v-list-item-subtitle>
+                    <v-list-item-subtitle class="mt-2">
+                      <p>Integer nulla turpis, vehicula vel ante et, scelerisque facilisis sapien. Praesent sagittis blandit turpis, eu sollicitudin erat consectetur sed. Cras eget magna fermentum, sollicitudin erat consectetur.</p>
+                    </v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list>
+            </div>
+        </div>
     </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      starFilterIndex: 0,
+      starFilterList: [
+        {
+          name: 'Show All',
+          active: true
+        },
+        {
+          name: '5 Stars',
+          active: false,
+          icon: 'mdi-star'
+        },
+        {
+          name: '4 Stars',
+          active: false,
+          icon: 'mdi-star'
+        },
+        {
+          name: '3 Stars',
+          active: false,
+          icon: 'mdi-star'
+        },
+        {
+          name: '2 Stars',
+          active: false,
+          icon: 'mdi-star'
+        },
+        {
+          name: '1 Stars',
+          active: false,
+          icon: 'mdi-star'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.btn-start-filter {
+  margin-right: 10px;
+  background-color: #FAFAFA;
+  &last-child {
+      margin-right: 0px;
+  }
+}
+</style>
