c3bc17a2dac1622efb6711321e63c0b1f30157b0
aden trisna daud kurnia <aden.trisna@exaditama.id>
2020-04-16 11:08:42 +0700

    modal event calendar

--- src/views/Restaurant/components/SpecialBusinessHours.vue
+++ src/views/Restaurant/components/SpecialBusinessHours.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="px-4 py-6">
-        <h1 class="app-title">Special Business Hours</h1>
+        <h1 class="app-title mb-10">Special Business Hours</h1>
         <div>
             <v-sheet
             tile
@@ -38,9 +38,115 @@
                 :event-overlap-threshold="30"
                 :show-month-on-first="false"
                 @change="getEvents"
+                @click:date="dialog = true"
             ></v-calendar>
             </v-sheet>
         </div>
+        <v-row justify="center">
+          <v-dialog v-model="dialog" persistent width="450">
+            <v-card>
+              <div class="d-flex justify-space-between" style="
+                height: 52px;
+                box-shadow: inset 0px -1px 0px rgba(0, 0, 0, 0.05);
+                overflow: hidden;
+              ">
+                <v-card-title style="
+                 font-weight: 500;
+                 font-size: 16px;
+                ">Special Hour</v-card-title>
+                <v-icon @click="dialog = false" class="mr-3">mdi-close</v-icon>
+              </div>
+
+              <v-list two-line subheader>
+                <v-list-item>
+                  <v-list-item-avatar>
+                    <v-icon>mdi-chevron-double-left</v-icon>
+                  </v-list-item-avatar>
+
+                  <v-list-item-content class="justify-center">
+                    <v-list-item-title class="text-center" v-text="'Friday'"></v-list-item-title>
+                    <v-list-item-subtitle class="text-center" v-text="'20 April 2020'"></v-list-item-subtitle>
+                  </v-list-item-content>
+
+                  <v-list-item-action>
+                    <v-btn icon>
+                      <v-icon>mdi-chevron-double-right</v-icon>
+                    </v-btn>
+                  </v-list-item-action>
+                </v-list-item>
+
+                <v-list-item style="background-color:#F5F5F5; border-radius: 4px" class="mx-10">
+                  <v-list-item-content class="justify-center">
+                    <v-list-item-title v-text="'Set Enabled'"></v-list-item-title>
+                  </v-list-item-content>
+
+                  <v-list-item-action>
+                    <v-switch
+                      v-model="enabled"
+                    ></v-switch>
+                  </v-list-item-action>
+                </v-list-item>
+
+              </v-list>
+
+              <div class="px-5 mt-5">
+                <p class="app-subtitle">Set Business Date</p>
+                <v-row>
+                  <v-col cols="6">
+                    <div>
+                        <p class="app-title-small ma-0">Start Date</p>
+                        <v-text-field
+                            single-line
+                            dense
+                            outlined
+                        ></v-text-field>
+                    </div>
+                  </v-col>
+                  <v-col cols="6">
+                    <div>
+                        <p class="app-title-small ma-0">End Date</p>
+                        <v-text-field
+                            single-line
+                            dense
+                            outlined
+                        ></v-text-field>
+                    </div>
+                  </v-col>
+                </v-row>
+              </div>
+              <div class="px-5">
+                <p class="app-subtitle">Set Business Time</p>
+                <v-row>
+                  <v-col cols="6">
+                    <div>
+                        <p class="app-title-small ma-0">Time Open</p>
+                        <v-text-field
+                            single-line
+                            dense
+                            outlined
+                        ></v-text-field>
+                    </div>
+                  </v-col>
+                  <v-col cols="6">
+                    <div>
+                        <p class="app-title-small ma-0">Time End</p>
+                        <v-text-field
+                            single-line
+                            dense
+                            outlined
+                        ></v-text-field>
+                    </div>
+                  </v-col>
+                </v-row>
+              </div>
+
+              <v-card-actions class="flex-column">
+                <v-btn color="#FDB526" dark block @click="dialog = false">Save Changes</v-btn>
+                <v-btn color="red" text block @click="dialog = false">Delete</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-row>
     </div>
 </template>
 
@@ -49,6 +155,7 @@ import * as moment from 'moment'
 export default {
   data () {
     return {
+      dialog: false,
       value: moment().format('YYYY-MM-DD'),
       mode: 'stack',
       type: 'month',
@@ -71,21 +178,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.my-event {
-  overflow: hidden;
-  width: 20px;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  border-radius: 2px;
-  background-color: #1867c0;
-  color: #FF0000;
-  border: 1px solid #1867c0;
-  font-size: 12px;
-  padding: 3px;
-  cursor: pointer;
-  margin-bottom: 1px;
-  left: 4px;
-  margin-right: 8px;
-  position: relative;
-}
 </style>
