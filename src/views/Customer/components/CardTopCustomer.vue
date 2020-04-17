6ae6b6795b8313eca6bcd60e38325714f0d5bdb6
aden trisna daud kurnia <aden.trisna@exaditama.id>
2020-04-17 14:48:50 +0700

    fix: setting chell paddingc

--- src/views/Customer/components/CardTopCustomer.vue
+++ src/views/Customer/components/CardTopCustomer.vue
@@ -1,21 +1,21 @@
 <template>
   <div>
-    <v-card outlined>
+    <v-card class="pb-6" outlined>
       <v-card-title class="app-subtitle">Top Customer</v-card-title>
       <v-simple-table>
         <template v-slot:default>
           <thead style="background-color:#FDB52630">
             <tr>
-              <th class="text-left" style="color:#FDB526">Customer ID</th>
-              <th class="text-left" style="color:#FDB526">Total Transaction</th>
-              <th class="text-left" style="color:#FDB526">Transaction Amount</th>
+              <th class="text-left py-5" style="color:#FDB526">Customer ID</th>
+              <th class="text-left py-5" style="color:#FDB526">Total Transaction</th>
+              <th class="text-left py-5" style="color:#FDB526">Transaction Amount</th>
             </tr>
           </thead>
           <tbody>
             <tr v-for="item in data" :key="item.name">
-              <td>{{ item.customer_id }}</td>
-              <td>{{ item.total_transaction }}</td>
-              <td>{{ item.transaction_amount }}</td>
+              <td class="py-5">{{ item.customer_id }}</td>
+              <td class="py-5">{{ item.total_transaction }}</td>
+              <td class="py-5">{{ item.transaction_amount }}</td>
             </tr>
           </tbody>
         </template>
