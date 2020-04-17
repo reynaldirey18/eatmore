bab682dfaae455ef0cbd295598225392a627b96f
aden trisna daud kurnia <aden.trisna@exaditama.id>
2020-04-17 13:37:26 +0700

    add: Card Top Customer

---
+++ src/views/Customer/components/CardTopCustomer.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <v-card outlined>
+      <v-card-title class="app-subtitle">Top Customer</v-card-title>
+      <v-simple-table>
+        <template v-slot:default>
+          <thead style="background-color:#FDB52630">
+            <tr>
+              <th class="text-left" style="color:#FDB526">Customer ID</th>
+              <th class="text-left" style="color:#FDB526">Total Transaction</th>
+              <th class="text-left" style="color:#FDB526">Transaction Amount</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in data" :key="item.name">
+              <td>{{ item.customer_id }}</td>
+              <td>{{ item.total_transaction }}</td>
+              <td>{{ item.transaction_amount }}</td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card Top Customer',
+  props: ['data']
+}
+</script>
--- src/views/Customer/views/Summary/index.vue
+++ src/views/Customer/views/Summary/index.vue
@@ -1,79 +1,68 @@
 <template>
   <div>
     <h1 class="app-title mb-5">Customer Overview</h1>
-    <v-card
-        class="mx-auto mt-3"
-        outlined
-    >
-      <div class="d-flex">
-        <v-tabs height="112" slider-color="#FDB526">
-          <v-tab class="tab-card-chart">
-            <p class="text-capitalize">Unique Customer</p>
-            <p class="bold">780</p>
-            <p class="green">+240 (+20%)</p>
-          </v-tab>
-          <v-tab class="tab-card-chart">
-            <p class="text-capitalize">Total Customer</p>
-            <p class="bold">820</p>
-            <p class="green">+340 (+40%)</p>
-          </v-tab>
-          <div class="d-flex align-self-center" style="position: absolute; right: 24px">
-            <v-btn outlined color="#E6E6E6" class="mr-6">
-              <div class="d-flex justify-space-between">
-                <span class="black60 text-capitalize">Monthly</span>
-                <v-icon class="ml-3" color="#FDB526" small>mdi-radiobox-marked</v-icon>
-              </div>
-            </v-btn>
-            <v-btn outlined color="#E6E6E6">
-              <div class="d-flex justify-space-between">
-                <span class="black60 text-capitalize">Yearly</span>
-                <v-icon class="ml-3" small>mdi-radiobox-blank</v-icon>
-              </div>
-            </v-btn>
-          </div>
-          <v-tab-item>
-            <chart-customer-growth-monthly></chart-customer-growth-monthly>
-          </v-tab-item>
-          <v-tab-item>
-            <chart-customer-growth-monthly></chart-customer-growth-monthly>
-          </v-tab-item>
-        </v-tabs>
-      </div>
-    </v-card>
+    <card-customer-summary></card-customer-summary>
+    <v-row>
+      <v-col cols="6">
+        <card-top-customer :data="topCustomer"></card-top-customer>
+      </v-col>
+    </v-row>
   </div>
 </template>
 <script>
-import ChartCustomerGrowthMonthly from '@/views/Customer/components/ChartCustomerGrowthMonthly'
+import CardCustomerSummary from '@/views/Customer/components/CardCustomerSummary'
+import CardTopCustomer from '@/views/Customer/components/CardTopCustomer'
 export default {
   name: 'Customer Summary',
   components: {
-    ChartCustomerGrowthMonthly
+    CardCustomerSummary,
+    CardTopCustomer
+  },
+  data () {
+    return {
+      topCustomer: [
+        {
+          customer_id: 'DarkSider1',
+          total_transaction: 41,
+          transaction_amount: 'Rp 7.300.000'
+        },
+        {
+          customer_id: 'the_bings',
+          total_transaction: 78,
+          transaction_amount: 'Rp 16.300.000'
+        },
+        {
+          customer_id: 'DarkSider1',
+          total_transaction: 41,
+          transaction_amount: 'Rp 7.300.000'
+        },
+        {
+          customer_id: 'Bings',
+          total_transaction: 78,
+          transaction_amount: 'Rp 16.380.000'
+        },
+        {
+          customer_id: 'Bingsss',
+          total_transaction: 41,
+          transaction_amount: 'Rp 7.300.000'
+        },
+        {
+          customer_id: 'the_bings',
+          total_transaction: 78,
+          transaction_amount: 'Rp 16.300.000'
+        },
+        {
+          customer_id: 'DarkSider1',
+          total_transaction: 41,
+          transaction_amount: 'Rp 7.300.000'
+        },
+        {
+          customer_id: 'the_bings',
+          total_transaction: 78,
+          transaction_amount: 'Rp 16.300.000'
+        }
+      ]
+    }
   }
 }
 </script>
-
-<style lang="scss" scoped>
-.tab-card-chart {
-    display: block;
-    height: 112px;
-    width: 239px;
-    padding-left: 50px;
-    padding-top: 10px;
-  p {
-    font-weight: normal;
-    font-size: 14px;
-    line-height: 0px;
-    text-align: left;
-    color: #000000;
-    margin-top: 24px;
-  }
-  p.bold {
-    font-weight: 500;
-    font-size: 22px;
-  }
-  p.green {
-    color: #1DBA81;
-    background-color: transparent !important;
-  }
-}
-</style>
