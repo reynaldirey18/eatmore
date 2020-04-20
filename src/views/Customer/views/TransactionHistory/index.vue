b3ce057b89affa039d9372cc82424d9c52978c0b
aden trisna daud kurnia <aden.trisna@exaditama.id>
2020-04-20 08:30:33 +0700

    init feat Loyalty Program & Transaction History

---
+++ src/views/Customer/views/TransactionHistory/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-card class="pb-6" outlined>
+      <v-card-title class="app-title">Transaction Reports</v-card-title>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TransactionHistory',
+  data () {
+    return {
+    }
+  }
+}
+</script>
