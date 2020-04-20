4d18b25314beb1b7a2f9c9476aa5cd973016823b
aden trisna daud kurnia <aden.trisna@exaditama.id>
2020-04-20 15:55:14 +0700

    feat(add reward): init page

---
+++ src/views/Customer/components/CardAddReward.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="d-flex">
+      <h1 class="app-title mb-5">Add Rewards</h1>
+      <v-spacer></v-spacer>
+      <v-btn color="#FDB526" @click.prevent="handleCreateReward" dark><span class="text-capitalize">Create Rewards</span></v-btn>
+    </div>
+    <v-card class="pb-6" outlined>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoyaltyRewardCard',
+  data () {
+    return {
+    }
+  },
+  methods: {
+    handleCreateReward () {
+      this.$emit('setAddReward', false)
+    }
+  }
+}
+</script>
