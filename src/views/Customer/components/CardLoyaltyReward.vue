fc3d727dbfca926bc6a9a8ad493f511155dcaec2
aden trisna daud kurnia <aden.trisna@exaditama.id>
2020-04-20 10:58:22 +0700

    feat(customer loyalty program): card reward

---
+++ src/views/Customer/components/CardLoyaltyReward.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <h1 class="app-title mb-5">Reward</h1>
+    <v-card class="pb-6" outlined>
+      <v-simple-table>
+        <template v-slot:default>
+          <thead style="background-color:#FDB52630">
+            <tr>
+              <th class="text-left py-5" style="color:#FDB526">Title</th>
+              <th class="text-left py-5" style="color:#FDB526">Min Transaction</th>
+              <th class="text-left py-5" style="color:#FDB526">Start Period</th>
+              <th class="text-left py-5" style="color:#FDB526">End Period</th>
+              <th class="text-left py-5" style="color:#FDB526">Reward</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="reward in rewardData" :key="reward.name">
+              <td class="py-5">{{ reward.title }}</td>
+              <td class="py-5">{{ reward.min_transaction }}</td>
+              <td class="py-5">{{ reward.start_period }}</td>
+              <td class="py-5">{{ reward.end_period }}</td>
+              <td class="py-5">{{ reward.reward }}</td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoyaltyReward Card',
+  data () {
+    return {
+      rewardData: [
+        {
+          title: '50% Cashback',
+          min_transaction: 'Rp. 100.000',
+          start_period: 'Mar, 3, 2020',
+          end_period: 'Dec, 19, 2020',
+          reward: 'Voucher'
+        },
+        {
+          title: '50% Cashback',
+          min_transaction: 'Rp. 100.000',
+          start_period: 'Mar, 3, 2020',
+          end_period: 'Dec, 19, 2020',
+          reward: 'Voucher'
+        },
+        {
+          title: '50% Cashback',
+          min_transaction: 'Rp. 100.000',
+          start_period: 'Mar, 3, 2020',
+          end_period: 'Dec, 19, 2020',
+          reward: 'Voucher'
+        }
+      ]
+    }
+  }
+}
+</script>
