f12c3d91c942342200c2d9ec34254455d6ee81e5
Fikri Razzaq <fikri.razzaq@exaditama.id>
2020-04-20 09:15:20 +0700

    fix(dashboard): fix sales summary layout judulnya.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Injek tidak ya?"/>
+    <HelloWorld msg="Injek INJEKINJEK?"/>
   </div>
 </template>
 
