b1f62b469e33bcb73fefe97addba5131b7989d93
aden trisna daud kurnia <aden.trisna@exaditama.id>
2020-04-20 14:23:47 +0700

    feat(customer): add button filter and button export

--- src/views/Customer/components/CardEligibleCustomer.vue
+++ src/views/Customer/components/CardEligibleCustomer.vue
@@ -1,6 +1,68 @@
 <template>
   <div>
     <v-card class="pb-6" outlined>
+      <div class="d-flex">
+        <v-card-title class="app-subtitle">Eligible Customer</v-card-title>
+        <v-spacer></v-spacer>
+        <div class="pa-5">
+          <v-menu
+            v-model="menu"
+            :close-on-content-click="false"
+            :max-width="211"
+            offset-y
+          >
+            <template v-slot:activator="{ on }">
+              <v-btn
+                color="#FFFFFF"
+                v-on="on"
+              >
+                <span class="text-capitalize">Sorts & Filters</span>
+                <v-icon>mdi-chevron-down</v-icon>
+              </v-btn>
+            </template>
+
+            <v-card>
+              <v-list flat dense subheader>
+                <v-subheader>Sort By</v-subheader>
+                <v-radio-group v-model="sortBy" color="#FDB526" dense class="pa-0 ma-0">
+                  <v-list-item v-for="(option, i) in sortByOption" :key="option.text + i">
+                    <template v-slot:default>
+                      <v-list-item-action class="pa-0 ma-0 mr-2">
+                        <v-radio :value="option.text" color="#FDB526"></v-radio>
+                      </v-list-item-action>
+                      <v-list-item-content>
+                        <v-list-item-title>{{option.text}}</v-list-item-title>
+                      </v-list-item-content>
+                    </template>
+                  </v-list-item>
+                </v-radio-group>
+
+                <v-subheader>Filters</v-subheader>
+                <v-radio-group dense v-model="filter" class="pa-0 ma-0">
+                  <v-list-item v-for="(option, i) in filterOptions" :key="option.text + i">
+                    <template v-slot:default>
+                      <v-list-item-action class="pa-0 ma-0 mr-2">
+                        <v-radio :value="option.text" color="#FDB526"></v-radio>
+                      </v-list-item-action>
+
+                      <v-list-item-content>
+                        <v-list-item-title>{{option.text}}</v-list-item-title>
+                      </v-list-item-content>
+                    </template>
+                  </v-list-item>
+                </v-radio-group>
+              </v-list>
+              <v-divider></v-divider>
+              <v-card-actions>
+                <v-btn text @click="menu = false">Reset</v-btn>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" text @click="menu = false">Save</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-menu>
+          <v-btn class="ml-5" color="#FDB526" dark><span class="text-capitalize">Export</span></v-btn>
+        </div>
+      </div>
       <v-simple-table>
         <template v-slot:default>
           <thead>
@@ -52,6 +114,34 @@ export default {
   data () {
     return {
       page: 1,
+      menu: false,
+      sortBy: '',
+      filter: '',
+      sortByOption: [
+        {
+          text: 'Highest Transaction'
+        },
+        {
+          text: 'Lowest Transaction'
+        }
+      ],
+      filterOptions: [
+        {
+          text: 'Open'
+        },
+        {
+          text: 'Redeemed'
+        },
+        {
+          text: 'Expired'
+        },
+        {
+          text: 'Vouchers'
+        },
+        {
+          text: 'Promo'
+        }
+      ],
       rewardData: [
         {
           title: '50% Cashback',
