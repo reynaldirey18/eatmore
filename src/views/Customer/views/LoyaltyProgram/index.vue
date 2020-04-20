fb645e93a2df5289f52dfac92c0ae02ef69f761f
aden trisna daud kurnia <aden.trisna@exaditama.id>
2020-04-20 10:59:05 +0700

    feat(customer loyalty program): rendern component card reward

--- src/views/Customer/views/LoyaltyProgram/index.vue
+++ src/views/Customer/views/LoyaltyProgram/index.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-    <h1 class="app-title mb-5">Reward</h1>
+    <card-loyalty-reward></card-loyalty-reward>
   </div>
 </template>
 
 <script>
+import CardLoyaltyReward from '@/views/Customer/components/CardLoyaltyReward'
 export default {
   name: 'LoyaltyProgram',
+  components: {
+    CardLoyaltyReward
+  },
   data () {
   }
 }
