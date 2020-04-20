f3fa5f220cdbb5a8c8157093a30f38142ee77f49
aden trisna daud kurnia <aden.trisna@exaditama.id>
2020-04-20 15:57:00 +0700

    feat(add reward): handling display add or list

--- src/views/Customer/views/LoyaltyProgram/index.vue
+++ src/views/Customer/views/LoyaltyProgram/index.vue
@@ -1,26 +1,38 @@
 <template>
   <div>
-    <div class="mb-8">
-      <card-loyalty-reward></card-loyalty-reward>
+    <div v-if="addReward" key="add Reward">
+      <card-add-reward @setAddReward="setAddReward"></card-add-reward>
     </div>
-    <div class="mb-8">
-      <card-eligible-customer></card-eligible-customer>
+    <div v-else key="add Reward Off">
+      <div class="mb-8">
+        <card-loyalty-reward @setAddReward="setAddReward"></card-loyalty-reward>
+      </div>
+      <div class="mb-8">
+        <card-eligible-customer></card-eligible-customer>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import CardLoyaltyReward from '@/views/Customer/components/CardLoyaltyReward'
+import CardAddReward from '@/views/Customer/components/CardAddReward'
 import CardEligibleCustomer from '@/views/Customer/components/CardEligibleCustomer'
 export default {
   name: 'LoyaltyProgram',
   components: {
     CardLoyaltyReward,
+    CardAddReward,
     CardEligibleCustomer
   },
   data () {
     return {
-      data: ''
+      addReward: false
+    }
+  },
+  methods: {
+    setAddReward (curent) {
+      this.addReward = curent
     }
   }
 }
