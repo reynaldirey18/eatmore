54aca07de1e0cf0c2817c663027187b550f70bff
aden trisna daud kurnia <aden.trisna@exaditama.id>
2020-04-21 16:21:35 +0700

    dummy list

--- src/views/Customer/views/TransactionHistory/index.vue
+++ src/views/Customer/views/TransactionHistory/index.vue
@@ -1,19 +1,54 @@
 <template>
   <div>
     <card-transaction-report></card-transaction-report>
+    <div class="mt-8">
+      <v-card>
+        <div class="d-flex">
+          <v-card-title class="app-subtitle">Transaction History</v-card-title>
+          <v-spacer></v-spacer>
+        </div>
+        <app-datatable
+          name="Customer"
+          :headers="headers"
+          url="https://reqres.in/api/users"
+          :perPage="10"
+        >
+        </app-datatable>
+      </v-card>
+    </div>
   </div>
 </template>
 
 <script>
+import AppDatatable from '@/components/AppDataTable'
 import CardTransactionReport from '@/views/Customer/components/CardTransactionReport'
 export default {
   name: 'TransactionHistory',
   components: {
+    AppDatatable,
     CardTransactionReport
   },
   data () {
     return {
-      data: ''
+      headers: [
+        {
+          text: 'Order ID',
+          value: 'id'
+        },
+        {
+          text: 'Date',
+          value: 'email'
+        },
+        {
+          text: 'Transaction Amount',
+          value: 'transaction_amount'
+        },
+        {
+          text: 'Status',
+          value: 'status'
+        }
+      ],
+      items: []
     }
   }
 }
