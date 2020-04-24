030002796a7ca405ebb3002fd31237d91e6ab11c
Angga Nurhiman <nurhiman_a@apps.ipb.ac.id>
2020-04-24 15:51:09 +0700

    feat(Promo): Submenu list in sidebar

--- src/router/nav.js
+++ src/router/nav.js
@@ -102,7 +102,19 @@ export const mainMenu = [
   {
     icon: 'mdi-timetable',
     name: 'Promo',
-    children: []
+    children: [
+      {
+        name: 'New Promo',
+        to: '/promo/new-promo'
+      },
+      {
+        name: 'Edit Promo',
+        to: '/promo/edit-promo'
+      },
+      {
+        name: 'Text'
+      }
+    ]
   },
   {
     icon: 'mdi-account-circle',
