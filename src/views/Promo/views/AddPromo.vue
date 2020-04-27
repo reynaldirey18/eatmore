92d1624eb1af415f6b1cab8bc91be76b1575e8d5
Angga Nurhiman <nurhiman_a@apps.ipb.ac.id>
2020-04-27 17:02:34 +0700

    feat(Promo): Add promo section condition and terms

--- src/views/Promo/views/AddPromo.vue
+++ src/views/Promo/views/AddPromo.vue
@@ -117,6 +117,49 @@
             </div>
           </v-col>
         </v-row>
+        <v-row class="condition">
+          <v-col cols="4">
+            <p class="label-form">Promo Conditional</p>
+            <v-select
+              :items="condition"
+              outlined
+              dense
+            ></v-select>
+          </v-col>
+          <v-col cols="4">
+            <p class="label-form">Minimum Purchase</p>
+            <v-form ref="form">
+              <v-text-field
+                v-model="purchase"
+                placeholder="Eg. 17.000"
+                outlined
+                dense
+              >
+              </v-text-field>
+            </v-form>
+          </v-col>
+          <v-col cols="4">
+            <p class="label-form">Promo Based</p>
+            <v-form ref="form">
+              <v-text-field
+                v-model="promoBased"
+                placeholder="Eg. 17.000"
+                outlined
+                dense
+              >
+              </v-text-field>
+            </v-form>
+          </v-col>
+        </v-row>
+        <p class="label-form">Promo Terms</p>
+        <v-textarea
+        outlined
+        dense
+        placeholder="Eg. Minimum Order Rp. 150.000"
+        ></v-textarea>
+      </div>
+      <div class="inform pa-4 pb-1">
+        <p class="app-subtitle-small">If the merchant uses a promo based on a minimum transaction, then automatically all products will be deducted according to the transaction filled by the merchant</p>
       </div>
     </v-card>
   </div>
@@ -133,6 +176,7 @@ export default {
   data () {
     return {
       type: ['Premium', 'Regular'],
+      condition: ['Promo By Minimum Transaction', 'Promo By Each Product'],
       title: null,
       image: null,
       startTime: {
@@ -174,11 +218,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.v-text-field {
-  .v-input__control {
-    .v-input__slot {
-      background-color: #FAFAFA !important;
-    }
-  }
+.condition {
+  margin-top: -30px;
+  margin-bottom: -20px;
+}
+.inform {
+  background-color: #F5F5F5;
 }
 </style>
