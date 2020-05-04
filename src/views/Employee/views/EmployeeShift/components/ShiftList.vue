fef8ba617cc8a7f5c28ec36ec835c835cac9b5a2
Angga Nurhiman <nurhiman_a@apps.ipb.ac.id>
2020-05-04 10:21:16 +0700

    feat(Employee): Data table for employee shift list

--- src/views/Employee/views/EmployeeShift/components/ShiftList.vue
+++ src/views/Employee/views/EmployeeShift/components/ShiftList.vue
@@ -15,12 +15,91 @@
       outlined
       hide-details
     ></v-text-field>
-    <div class="not-found d-flex flex-column">
+    <div class="not-found d-flex flex-column" v-if="shiftList.length < 1">
       <img src="@/assets/img/employee_not_found.png" alt="Employee Not Found">
       <p class="text-bold mt-4">You Don’t Have Any Employee Shift</p>
     </div>
+    <div class="table mt-6" v-else>
+      <v-data-table
+        :headers="headers"
+        :items="shiftList"
+        :page.sync="page"
+        :items-per-page="itemsPerPage"
+        hide-default-footer
+        class="elevation-1"
+        @page-count="pageCount = $event"
+      >
+        <template v-slot:item.actions="{item}">
+          <div class="d-flex flex-row align-center justify-end">
+            <v-btn text color="#3D87F4" small @click="goToEdit(item)" class="mr-1">Quick Edit</v-btn>
+            <v-btn icon color="#CCCCCC" @click="goToEdit(item)">
+              <v-icon>mdi-dots-horizontal</v-icon>
+            </v-btn>
+          </div>
+        </template>
+      </v-data-table>
+      <div class="d-flex justify-space-between mt-3">
+        <div class="ma-4" v-if="itemsPerPage >= shiftList.length">
+          Show {{shiftList.length}} of {{shiftList.length}} Employee
+        </div>
+        <div class="ma-4" v-else>
+          Show {{itemsPerPage}} of {{shiftList.length}} Employee
+        </div>
+        <div>
+        <v-pagination
+          v-model="page"
+          color="#FDB526"
+          :length="pageCount">
+        </v-pagination>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'AppShiftList',
+  data () {
+    return {
+      page: 1,
+      pageCount: 0,
+      itemsPerPage: 10,
+      show1: false,
+      headers: [
+        { text: 'Employee Name', value: 'name' },
+        { text: 'Division', value: 'division' },
+        { text: 'Shift', value: 'shift' },
+        { text: 'Outlet', value: 'outlet' },
+        { text: '', align: 'right', value: 'actions', sortable: false }
+      ],
+      shiftList: [
+        {
+          id: 1,
+          name: 'Theresa Bell',
+          division: 'Cashier',
+          shift: '08:00 - 12:00',
+          outlet: 'Outlet 1'
+        },
+        {
+          id: 2,
+          name: 'Steve Rogers',
+          division: 'Cashier',
+          shift: '08:00 - 12:00',
+          outlet: 'Outlet 1'
+        },
+        {
+          id: 3,
+          name: 'Camelia',
+          division: 'Cashier',
+          shift: '08:00 - 12:00',
+          outlet: 'Outlet 1'
+        }
+      ]
+    }
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 .not-found {
