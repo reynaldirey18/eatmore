5c1f6c0fe462e28c06afb62a278999accb0a9c80
Angga Nurhiman <nurhiman_a@apps.ipb.ac.id>
2020-05-06 17:13:07 +0700

    feat(Vuex): Fix mutations test

--- src/store/modules/Auth/index.js
+++ src/store/modules/Auth/index.js
@@ -1,14 +1,8 @@
 import actions from './actions'
-// import mutations from './mutations'
+import mutations from './mutations'
 
 const state = {
   token: 'asdas123213kjdk'
-}
-
-const mutations = {
-  SET_TOKEN (state, val) {
-    state.token = val
-  }
 }
 
 export default {
