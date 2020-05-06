7cfb0630207c5c0f04ffb9e673679fe741a84b2d
Angga Nurhiman <nurhiman_a@apps.ipb.ac.id>
2020-05-06 14:14:34 +0700

    feat(Employee): Data table for holidays list

--- src/views/Employee/views/Holiday/index.vue
+++ src/views/Employee/views/Holiday/index.vue
@@ -11,6 +11,41 @@
         <p class="mt-2">Add some holiday now to make your employee keep productive</p>
       </div>
     </v-card>
+    <v-card class="mt-6" outlined v-else>
+      <div class="table">
+        <v-data-table
+          :headers="headers"
+          :items="holidays"
+          :page.sync="page"
+          :items-per-page="itemsPerPage"
+          hide-default-footer
+          class="elevation-1"
+          @page-count="pageCount = $event"
+        >
+          <template v-slot:item.actions="{item}">
+            <div class="d-flex flex-row align-center justify-end">
+              <v-btn text color="#3D87F4" small class="mr-1" @click="edit(item)">Edit</v-btn>
+              <v-btn text color="#F32626">Delete</v-btn>
+            </div>
+          </template>
+        </v-data-table>
+        <div class="d-flex justify-space-between mt-3">
+          <div class="ma-4" v-if="itemsPerPage >= holidays.length">
+            Show {{holidays.length}} of {{holidays.length}} Employee
+          </div>
+          <div class="ma-4" v-else>
+            Show {{itemsPerPage}} of {{holidays.length}} Employee
+          </div>
+          <div>
+          <v-pagination
+            v-model="page"
+            color="#FDB526"
+            :length="pageCount">
+          </v-pagination>
+          </div>
+        </div>
+      </div>
+    </v-card>
   </div>
 </template>
 
@@ -18,7 +53,35 @@
 export default {
   data () {
     return {
-      holidays: []
+      page: 1,
+      pageCount: 0,
+      itemsPerPage: 10,
+      show1: false,
+      headers: [
+        { text: 'Holiday Name', value: 'name' },
+        { text: 'Holiday Date', value: 'date' },
+        { text: '', align: 'end', value: 'actions', sortable: false }
+      ],
+      holidays: [
+        {
+          id: 1,
+          number: 1,
+          name: 'New Year',
+          date: 'Jan 1, 2020'
+        },
+        {
+          id: 2,
+          number: 2,
+          name: 'May Day',
+          date: 'May 1, 2020'
+        },
+        {
+          id: 3,
+          number: 3,
+          name: 'Good Day',
+          date: 'June 9, 2020'
+        }
+      ]
     }
   }
 }
