a2402470de9a2212d3a977a5012119e68a3c93c0
Angga Nurhiman <nurhiman_a@apps.ipb.ac.id>
2020-05-11 15:41:13 +0700

    feat(Employee): Attendance - leaves list (no data)

---
+++ src/views/Employee/views/AttendanceLeaves/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="mt-4 d-flex flex-row justify-space-between align-center">
+      <h1 class="app-title">Attendance - Leaves</h1>
+      <v-btn color="#FDB526" dark small>Add New Leave</v-btn>
+    </div>
+    <v-card class="pa-6 mt-6 d-flex" outlined min-height="400px" v-if="attendance.length < 1">
+      <div class="not-found d-flex flex-column">
+        <img src="@/assets/img/illustration2.png" alt="No Attendance">
+        <p class="text-bold mt-2">No Attendance Yet!</p>
+      </div>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      attendance: []
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.not-found {
+  margin: auto;
+  width: 100%;
+  img {
+    margin: auto;
+    width: 30%;
+  }
+  p {
+    margin: auto;
+    color: black;
+  }
+}
+</style>
