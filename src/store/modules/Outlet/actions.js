0385e77b0c90d9c46996330cd729006d42c1d441
Angga Nurhiman <nurhiman_a@apps.ipb.ac.id>
2020-05-15 09:03:23 +0700

    feat(Outlet Management): List outlet API implementation

--- src/store/modules/Outlet/actions.js
+++ src/store/modules/Outlet/actions.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import axios from 'axios'
+import Cookies from 'js-cookie'
 
 Vue.use(axios)
+
+const token = Cookies.get('token')
 
 const registOutlet = ({ state }) => {
   return new Promise((resolve, reject) => {
@@ -14,6 +17,23 @@ const registOutlet = ({ state }) => {
   })
 }
 
+const getList = ({ commit }) => {
+  return new Promise((resolve, reject) => {
+    axios.get('http://api.eatmore.id/outlet_service/', {
+      headers: {
+        Authorization: 'Bearer ' + token
+      }
+    })
+      .then(response => {
+        console.log(response)
+        commit('SET_LIST', response.data.data)
+      }, error => {
+        reject(error)
+      })
+  })
+}
+
 export default {
-  registOutlet
+  registOutlet,
+  getList
 }
