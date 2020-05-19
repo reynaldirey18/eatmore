40c75e9fde18200fb916723567ff30a85f3482ec
Angga Nurhiman <nurhiman_a@apps.ipb.ac.id>
2020-05-19 09:32:26 +0700

    feat(Authentication): Auto logout when token expired

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import Cookie from 'js-cookie'
+import Cookies from 'js-cookie'
 
 const { VUE_APP_API_URL } = process.env
 
@@ -7,18 +7,33 @@ const client = axios.create({
   baseURL: VUE_APP_API_URL
 })
 
-client.interceptors.request.use(
-  requestConfig => {
-    const originalConfig = requestConfig
-    const userToken = Cookie.get('token')
-    originalConfig.headers.Authorization = `Bearer ${userToken}`
-
-    return originalConfig
-  },
-  error => {
-    return Promise.reject(error)
+client.interceptors.response.use((response) => {
+  if (response.status === 401) {
+    Cookies.remove('token')
+    this.$router.push('/login')
   }
-)
+  return response
+}, (error) => {
+  if (error.response && error.response.data) {
+    Cookies.remove('token')
+    this.$router.push('/login')
+    return Promise.reject(error.response.data)
+  }
+  return Promise.reject(error.message)
+})
+
+// client.interceptors.request.use(
+//   requestConfig => {
+//     const originalConfig = requestConfig
+//     const userToken = Cookies.get('token')
+//     originalConfig.headers.Authorization = `Bearer ${userToken}`
+
+//     return originalConfig
+//   },
+//   error => {
+//     return Promise.reject(error)
+//   }
+// )
 
 export {
   client
